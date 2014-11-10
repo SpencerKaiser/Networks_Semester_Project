89960043f3c0d06412ac249e2a4dfc58154b74b4
SpencerKaiser <skaiser@me.com>
2014-11-10 15:14:49 -0600

    Updating main to include functionality for encoding/decoding with hamming and crc and keeping track of the results

--- code/main.py
+++ code/main.py
@@ -1,19 +1,79 @@
 import hamming
 import crc
 
+
 def main():
-    print("garbage")
     packets = open('../data/packets.txt', 'r')
 
-    badDecodes = 0
+    # CRC VARIABLES
+    crcTransmissions = 0
+    crcRetransmissions = 0
+    crcUndetectedErrors = 0
 
+    # HAMMING VARIABLES
+    hammingTransmissions = 0
+    hammingRetransmissions = 0
+    hammingCorrections = 0
+    hammingUndetectedErrors = 0
+
+    # PACKET ANALYSIS
     for packet in packets:
-        packet = packet[0:len(packet)-1]        
-        encodedPacket = crc.encode(packet)
-        if crc.decode(encodedPacket) == False:
-            badDecodes
+        packet = packet[:len(packet)-1]        # Remove the return carriage
 
-    print "Number of Bad Decodes: "+str(badDecodes)
+        # CRC
+        success = False
+        crcEncodedPacket = crc.encode(packet)
+
+        while success == False:                           # Continue until the packet is accurately received
+            # TODO: PASS TO NOISE FUNCTION HERE
+            crcNoisePacket = crcEncodedPacket
+
+            crcTransmissions += 1
+            success = True
+            
+            if crc.decode(crcNoisePacket) == False:       # If error(s) exist
+                crcRetransmissions += 1
+                success = False
+            elif crcEncodedPacket != crcNoisePacket:      # Error occured and CRC didn't catch it
+                crcUndetectedErrors += 1
+
+        # HAMMING
+        success = False
+        hammingEncodedPacket = hamming.encode(packet)
+
+        while success == False:                           # Continue until the packet is accurately received
+            # TODO: PASS TO NOISE FUNCTION HERE
+            hammingNoisePacket = hammingEncodedPacket
+
+            hammingTransmissions += 1
+            success = True
+            
+            if hamming.decode(hammingNoisePacket) == False:       # If error(s) exist
+                hammingRetransmissions += 1
+                success = False
+            elif hammingEncodedPacket != hammingNoisePacket:      # Error occured and CRC didn't catch it
+                hammingUndetectedErrors += 1
+
+
+    # SUMMARY
+    print "\n"
+
+    print "CRC ANALYSIS:"
+    print "\tTransmissions: "+str(crcTransmissions)
+    print "\tRetransmissions: "+str(crcRetransmissions)+" ~ "+str(crcRetransmissions/crcTransmissions*100)+"%"
+    print "\tUndetected Errors: "+str(crcUndetectedErrors)
+    
+    print "\n"
+
+    print "HAMMING ANALYSIS"
+    print "\tTransmissions: "+str(hammingTransmissions)
+    print "\tRetransmissions: "+str(hammingRetransmissions)+" ~ "+str(hammingRetransmissions/hammingTransmissions*100)+"%"
+    print "\tCorrections: "+str(hammingCorrections)
+    print "\tUndetected Errors: "+str(hammingUndetectedErrors)
+
+    print "\n"
+
+
         
 
 
