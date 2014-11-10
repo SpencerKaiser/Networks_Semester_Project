b80ccee11ad66a584a8a7c2c993b697d58aa42a9
lukeoglesbee <loglesbee@smu.edu>
2014-11-10 13:35:53 -0600

    v1.0 of burst noise

--- code/noise.py
+++ code/noise.py
@@ -4,10 +4,10 @@
 import numpy as np
 from sys import stdout
 
-__verbose = False
+__verbose = True
 
 def gaussian(signal, noise_ratio):
-    """Put signal through gaussian white noise fitler"""
+    """Put signal through gaussian white noise filter"""
     if noise_ratio < 0 or noise_ratio > 1:
         print "noise_ratio must be between 0 and 1"
         return False
@@ -29,3 +29,48 @@ def gaussian(signal, noise_ratio):
                 stdout.write(" ")
         print ""
     return "".join(str(x) for x in output)
+
+def burst(signal, noise_ratio, max_bit_flip=16):
+    """Put signal through burst noise filter"""
+    if noise_ratio < 0 or noise_ratio > 1:
+        print "noise_ratio must be between 0 and 1"
+        return False
+    # Generate noise
+    noise = np.random.rand(len(signal))
+    length = [int(x*max_bit_flip) for x in np.random.rand(len(signal))]
+    flip = map(lambda x: x < noise_ratio, noise)
+    i = 0
+    while i < len(flip):
+        if flip[i]:
+            j = i
+            i += length[i]
+            while j < len(flip) and j < i:
+                flip[j] = True
+                j += 1
+        i += 1
+    output = []
+    for f,s in zip(flip, signal):
+        if f:
+            output.append((int(s)+1)%2)
+        else:
+            output.append(int(s))
+    if __verbose:
+        for f in flip:
+            if f:
+                stdout.write("|")
+            else:
+                stdout.write(" ")
+        print ""
+    return "".join(str(x) for x in output)
+
+
+num = '0101000101010101010100110100101010111010101000101010101010010010010101010101010'
+print num
+num = burst(num, 0.05)
+
+
+
+
+
+
+print num
